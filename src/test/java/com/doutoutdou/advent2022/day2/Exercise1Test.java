2cf82f059371babf53d9c04660d8e254b05ed908
doudou <edouard.talma@gmail.com>
2022-12-02 07:08:43 +0100

    day2

---
+++ src/test/java/com/doutoutdou/advent2022/day2/Exercise1Test.java
@@ -0,0 +1,68 @@
+package com.doutoutdou.advent2022.day2;
+
+import com.doutoutdou.advent2022.utils.ExerciceUtils;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+
+import java.util.Objects;
+
+@Log4j2
+class Exercise1Test {
+
+    @Test
+    void version1() {
+        var data = ExerciceUtils.loadFromFileAsStringList("2");
+        long startTime = System.nanoTime();
+
+        int score = 0;
+        for (int i = 0; i < Objects.requireNonNull(data).size(); i++) {
+            score += getScore(data.get(i));
+        }
+
+        log.info("Duration " + (System.nanoTime() - startTime) + "ns");
+
+        System.out.println(score);
+    }
+
+    // https://stackoverflow.com/questions/24308146/why-is-a-combiner-needed-for-reduce-method-that-converts-type-in-java-8
+    @Test
+    void version2() {
+        var data = ExerciceUtils.loadFromFileAsStringList("2");
+        long startTime = System.nanoTime();
+
+        int score = data
+                .stream()
+                .reduce(0,
+                        (accumulatedInt, str) -> accumulatedInt + getScore(str),                 //accumulator
+                        Integer::sum); //combiner
+
+
+        log.info("Duration " + (System.nanoTime() - startTime) + "ns");
+
+        System.out.println(score);
+
+    }
+
+    private int getScore(String input) {
+        return switch (input) {
+            case "A Z" -> 3;
+            case "B X" -> 1;
+            case "C Y" -> 2;
+            case "A Y" -> 2 + 6;
+            case "C X" -> 1 + 6;
+            case "B Z" -> 3 + 6;
+            case "A X" -> 1 + 3;
+            case "B Y" -> 2 + 3;
+            case "C Z" -> 3 + 3;
+            default -> 0;
+        };
+    }
+    // Première solution + rapide
+//06:57:19.240 [main] INFO com.doutoutdou.advent2022.day2.Exercise1Test - Duration 961891ns
+//11873
+//        06:57:19.259 [main] INFO com.doutoutdou.advent2022.day2.Exercise1Test - Duration 2587629ns
+//11873
+
+}
+
+
---
+++ src/test/java/com/doutoutdou/advent2022/day2/Exercise2Test.java
@@ -0,0 +1,76 @@
+package com.doutoutdou.advent2022.day2;
+
+import com.doutoutdou.advent2022.utils.ExerciceUtils;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
+@Log4j2
+class Exercise2Test {
+
+    @Test
+    void version1() {
+        var data = ExerciceUtils.loadFromFileAsStringList("2");
+        long startTime = System.nanoTime();
+
+        int score = 0;
+        for (int i = 0; i < Objects.requireNonNull(data).size(); i++) {
+            score += getScore(data.get(i));
+        }
+
+        log.info("Duration " + (System.nanoTime() - startTime) + "ns");
+
+        System.out.println(score);
+    }
+
+    // https://stackoverflow.com/questions/24308146/why-is-a-combiner-needed-for-reduce-method-that-converts-type-in-java-8
+    @Test
+    void version2() {
+        var data = ExerciceUtils.loadFromFileAsStringList("2");
+        long startTime = System.nanoTime();
+
+        int score = data
+                .stream()
+                .reduce(0,
+                        (accumulatedInt, str) -> accumulatedInt + getScore(str),                 //accumulator
+                        Integer::sum); //combiner
+
+
+        log.info("Duration " + (System.nanoTime() - startTime) + "ns");
+
+        System.out.println(score);
+
+    }
+
+    // A Z ROCK
+    // B Y PAPER
+    // C Z SCISSORS
+    private int getScore(String input) {
+        return switch (input) {
+            case "A Z" -> 2 + 6;
+            case "B X" -> 1 + 0;
+            case "C Y" -> 3 + 3;
+            case "A Y" -> 1 + 3;
+            case "C X" -> 2 + 0;
+            case "B Z" -> 3 + 6;
+            case "A X" -> 3 + 0;
+            case "B Y" -> 2 + 3;
+            case "C Z" -> 1 + 6;
+            default -> 0;
+        };
+    }
+
+    // Première solution + rapide
+//07:07:25.625 [main] INFO com.doutoutdou.advent2022.day2.Exercise2Test - Duration 984439ns
+//12014
+//        07:07:25.647 [main] INFO com.doutoutdou.advent2022.day2.Exercise2Test - Duration 2755597ns
+//12014
+
+
+}
+
+
